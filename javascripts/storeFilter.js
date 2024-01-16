4ffec8061a22e64c7753689c4a55c7fc51f31721
kdev307 <kumardev30072002@gmail.com>
2024-01-16 20:19:31 +0530

    Filter Button Functionality Corrected

--- javascripts/storeFilter.js
+++ javascripts/storeFilter.js
@@ -3,28 +3,37 @@ const productItems = document.querySelectorAll(".product-item");
 
 filterContainer.addEventListener("click", (event) => {
     if (event.target.classList.contains("filter-item")) {
+        // Remove 'active' class from the previously active filter
         filterContainer.querySelector(".active").classList.remove("active");
+
+        // Add 'active' class to the clicked filter
         event.target.classList.add("active");
+
+        // Get the filter value from the data-filter attribute
         const filterValue = event.target.getAttribute("data-filter");
+
+        // Iterate over each product item
         productItems.forEach((item) => {
             if (item.classList.contains(filterValue) || filterValue === "all") {
-                if (filterValue != "all") {
+                // Show the 'filter-all' element when filter is not 'all'
+                if (filterValue !== "all") {
                     document.getElementById("filter-all").style.display = "block";
                     document.getElementById("filter-all").style.fontSize = "1.2rem";
-                }
-                if (filterValue === "all") {
+                } else {
+                    // Hide the 'filter-all' element when filter is 'all'
                     document.getElementById("filter-all").style.display = "none";
                 }
+                if (filterValue === "sports")
+                    document.getElementById("sport-cat").style.display = "flex";
+                else document.getElementById("sport-cat").style.display = "none";
+                // Remove 'hide' and add 'show' classes
                 item.classList.remove("hide");
                 item.classList.add("show");
             } else {
+                // Remove 'show' and add 'hide' classes
                 item.classList.remove("show");
                 item.classList.add("hide");
             }
         });
     }
 });
-
-// function disableFilter(){
-//     filterValue=
-// }
